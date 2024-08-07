0955b67e17a66b2d74b4e34879ca74a0c03e187c
albyei <albi_ariza_32rpl@student.smktelkom-mlg.sch.id>
2024-08-07 22:18:49 +0700

    Praktikum JavaScript

--- 4.js
+++ 4.js
@@ -1,19 +1,19 @@
 // mendefinisikan suku pertama dan rasio
-const sukuPertama =4;
-const rasio = 3;
-const jumlahSuku = 10;
+const sukuPertama = 4;   // suku pertama dari deret geometri
+const rasio = 3;        // rasio antara suku-suku dalam deret geometri
+const jumlahSuku = 10;  // jumlah suku yang ingin dihitung dalam deret
 
-// 
+// variabel untuk menyimpan total jumlah suku
 let totalJumlah = 0;
 
-//
+//perulangan untuk menghitung setiap suku dan menambahkannya ke total jumlah
 
 for (let i = 0; i < jumlahSuku; i++) {
     // menghitung suku ke-1 dari deret geometri
     const sukuke1 = sukuPertama * Math.pow(rasio, i);
-// menambahkan suku ke-1 ke total jumlah
-totalJumlah += sukuke1
-   
+    // menambahkan suku ke-1 ke total jumlah
+    totalJumlah += sukuke1
+
 }
-// menampilkan hasil
-console.log("Total jumlah 10 suku pertama dari deret geometri yaitu : ", totalJumlah);
+// menampilkan hasil akhir
+console.log("Total jumlah 10 suku pertama dari deret geometri yaitu : ", totalJumlah); 
